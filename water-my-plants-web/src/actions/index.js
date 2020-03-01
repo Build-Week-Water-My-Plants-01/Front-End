c047a69c00e00a4333ac25452b3f3b712925f452
Luis PM <luisp237@gmail.com>
2020-03-01 18:33:38 -0500

    set up Redux, axiosWithAuth, privateRoute component   *created actions, component, reducer, utils directory   * in utils created axiosWithAuth storing future token in local storage   *in utils created PrivateRoute component that will render protected route components   *created Redux Store at index.js Level   *initialized actions and redux with index.js file with dummy data

---
+++ water-my-plants-web/src/actions/index.js
@@ -0,0 +1,8 @@
+import axiosWithAuth from '../utils/axiosWithAuth';
+
+export const FETCH_DATA = 'FETCH_DATA';
+
+export const fetchPlants = () => dispatch => {
+    dispatch({ type: FETCH_DATA});
+    
+}
